8383f48639bf9d16e8e28899c59a8bbf9c2b8f12
gaopeng <priestch@gmail.com>
2018-07-03 22:40:44 +0800

    add more strict eslint rule and use prettier to format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,17 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/prettier'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-const': 'error',
+    'no-trailing-spaces': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-closing-bracket-spacing': 'error',
   },
   parserOptions: {
     parser: 'babel-eslint'
