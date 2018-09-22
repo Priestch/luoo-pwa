78a84f1d94719d3b5c20318c7384206c65189551
gaopeng <priestch@gmail.com>
2018-09-22 22:41:53 +0800

    add basic volume detail view

---
+++ src/views/VolumeDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <volume
+      v-if="volume"
+      :volume="volume" />
+  </div>
+</template>
+
+<script>
+import Volume from '@/components/Volume';
+import { fetchVolume } from '../api/volume.api';
+
+export default {
+  name: 'VolumeDetail',
+  components: {
+    Volume,
+  },
+  props: {
+    volumeId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      volume: null,
+    };
+  },
+  computed: {
+    id() {
+      return Number(this.volumeId);
+    },
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    async initialize() {
+      this.volume = await fetchVolume(this.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.volume {
+  margin: 0 auto;
+}
+</style>
